50e94e2412b0d1134607dc938ec8c4b9a77e0dba
Ayushi Jain <ayuj5571@gmail.com>
2023-07-12 18:37:34 +0530

    checking

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <>
+      <div className="container">
+        <img src="portfolioImg.jpeg" width="900px" height="700px" />
+      </div>
+    </>
+  );
+}
+
+export default Main;
